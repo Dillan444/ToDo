3efb93028d0e11e156f65594991e760716ea4997
Dillan444 <rojasdylan444@gmail.com>
2025-08-02 00:38:46 -0500

    Creation of the main basic components

---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Item #1</p>
+      <button>X</button>
+    </li>
+  )
+}
+
+export {TodoItem}
---
+++ src/TodoSearch.js
@@ -0,0 +1,7 @@
+function TodoSearch() {
+  return (
+    <input placeholder="Learn React"/>
+  )
+}
+
+export { TodoSearch }
