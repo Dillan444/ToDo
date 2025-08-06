5eecd43a11129f991f80326803ab91a2dcaa985b
Dillan444 <rojasdylan444@gmail.com>
2025-08-06 00:03:06 -0500

    Custom Icons

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,24 @@
+import {CompleteIcon} from './CompleteIcon'
+import {DeleteIcon} from './DeleteIcon'
 import './TodoItem.css'
 
 function TodoItem({text, completed, onComplete, onDelete}) {
   return (
     <li className="TodoItem">
-
-      <span className={`Icon Icon-check ${completed && 'Icon-check--active'}`}
+      <CompleteIcon 
+        completed={completed}
+        onComplete={onComplete}
+      />
+      {/* <span className={`Icon Icon-check ${completed && 'Icon-check--active'}`}
         onClick={onComplete}
-      >ok</span>
+      >ok</span> */}
 
       <p className={`TodoItem-p ${completed && 'TodoItem-p--complete'}`}>{text}</p>
-      <button className="Icon Icon-delete"
-      onClick={onDelete}>✖️</button>
+      <DeleteIcon
+        onDelete={onDelete}
+      />
+      {/* <button className="Icon Icon-delete"
+      onClick={onDelete}>✖️</button> */}
     </li>
   )
 }
